d4bcdfb2bc822e7b7089b6dab7bc2b5c73737091
mchung <test@test.com>
2024-09-04 18:06:50 +0900

    4번 문제 최적화

--- src/questionFour.java
+++ src/questionFour.java
@@ -5,22 +5,21 @@ public class questionFour {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int m = sc.nextInt();
         if(n == 0) {
             System.exit(0);
         }
+        int m = sc.nextInt();
         sc.nextLine();
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<String> list = new ArrayList<>(n);
         for(int i = 0; i < n; i++) {
             list.add(sc.nextLine());
         }
         for(String a: list) {
-            String result = "";
-            for(int i = 0; i < m; i++) {
-                char ch = a.charAt(i);
-                result = ch + result;
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < m; i++) {
+                result.append(a.charAt(i));
             }
-            System.out.println(result);
+            System.out.println(result.reverse().toString());
         }
     }
 }
